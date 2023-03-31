64cfb76af68c620f2cd371670d9f2d37ad9d290a
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-03-31 08:43:03 +0700

    add OperatorArray.php

---
+++ OperatorArray.php
@@ -0,0 +1,11 @@
+<?php
+  $first = ["first_name" => "Eko"];
+  $last = ["first_name" => "Budi", "last_name" => "Khannedy"];
+
+  $full = $first + $last;
+
+  $a = ["first_name" => "Eko", "last_name" => "Khannedy"];
+  $b = ["last_name" => "Khannedy", "first_name" => "Eko"];
+
+  var_dump($a == $b);
+  var_dump($a === $b);
