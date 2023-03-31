57dd6ad8d8032ed86d5c38b12a9b0a14a657e47e
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-03-31 09:06:15 +0700

    update ManipulasiString.php konversi ke number dan sebaliknya

--- ManipulasiString.php
+++ ManipulasiString.php
@@ -3,3 +3,12 @@
 
   echo "Nama : " . $name . PHP_EOL;
   echo "Nilai : " . 100 . "\n";
+
+  $valueString = (string)100;
+  var_dump($valueString);
+
+  $valueInt = (int)"100";
+  var_dump($valueInt);
+
+  $valueFloat = (float)"1.01";
+  var_dump($valueFloat);
