320bc3d50573d78e1085936611410a44e9594843
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-04-02 01:28:12 +0700

    update SwitchStatement.php switch statement syntax alternatif

--- SwitchStatement.php
+++ SwitchStatement.php
@@ -14,3 +14,17 @@
     default :
       echo "Mungkin anda salah jurusan" . PHP_EOL;
   }
+
+  switch($nilai) :
+    case "A" :
+      echo "Anda lulus dengan sangat baik" . PHP_EOL;
+      break;
+    case "B" :
+    case "C" :
+      echo "Anda lulus" . PHP_EOL;
+    case "D" :
+      echo "Anda tidak lulus" . PHP_EOL;
+      break;
+    default :
+      echo "Mungkin anda salah jurusan" . PHP_EOL;
+  endswitch;
