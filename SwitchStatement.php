31950223a16a8156715d322d53a5cb0abe155c36
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-04-02 01:25:15 +0700

    add SwitchStatement.php

---
+++ SwitchStatement.php
@@ -0,0 +1,16 @@
+<?php
+  $nilai = "A";
+
+  switch($nilai){
+    case "A" :
+      echo "Anda lulus dengan sangat baik" . PHP_EOL;
+      break;
+    case "B" :
+    case "C" :
+      echo "Anda lulus" . PHP_EOL;
+    case "D" :
+      echo "Anda tidak lulus" . PHP_EOL;
+      break;
+    default :
+      echo "Mungkin anda salah jurusan" . PHP_EOL;
+  }
