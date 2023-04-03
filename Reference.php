011be0b33a811046560775eedad47982299ff77d
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-04-03 22:32:31 +0700

    update Reference.php returning reference

--- Reference.php
+++ Reference.php
@@ -14,3 +14,15 @@
   increment($counter);
 
   echo $counter . PHP_EOL;
+
+  function &getValue()
+  {
+    static $value = 100;
+    return $value;
+  }
+
+  $a = &getValue();
+  $a = 200;
+  $b = &getValue();
+
+  echo $b . PHP_EOL;
